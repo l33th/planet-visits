6cace28348e4c093d6d23c5ccd2f38024f68dc5c
l33th <l33th@protonmail.ch>
2021-10-04 11:24:26 +0200

    Added List Component - Clean

--- src/App.js
+++ src/App.js
@@ -1,7 +1,5 @@
 import React, { useState } from "react";
 import VisitPlanets from "./components/VisitPlanets";
-import data from "./data";
-import List from "./components/List";
 
 const App = () => <VisitPlanets />;
 
