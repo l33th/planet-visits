88efe3f991419cc297917111eb44ce88834e346f
l33th <l33th@protonmail.ch>
2021-10-04 11:12:34 +0200

    Project Setup

---
+++ src/components/VisitPlanets.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const VisitPlanets = () => {
+    return (
+        <div>
+            <h1>Planets To Visit</h1>
+        </div>
+    )
+}
+
+export default VisitPlanets
---
+++ src/data.js
@@ -0,0 +1,42 @@
+export default [
+	{
+		id: 1,
+		name: "Kepler-22 b",
+		distance: "635 light-years from Earth",
+		description:
+			"A possible ocean world orbiting in the habitable zone—the region around a star where the temperature is right for liquid water, a requirement for life on Earth.",
+		image: "https://images.fineartamerica.com/images-medium-large-5/kepler-22b-marc-ward.jpg",
+	},
+	{
+		id: 2,
+		name: "GJ 15 A c",
+		distance: "12 light-years from Earth",
+		description:
+			"Nearby GJ 15 A c orbits a red-dwarf star just 11 light-years away with its companion, making them our nearest multi-planet neighbors discovered so far.",
+		image: "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse2.mm.bing.net%2Fth%3Fid%3DOIP.gq5ZPQInnibRGjZZYQw81AHaFj%26pid%3DApi&f=1",
+	},
+	{
+		id: 3,
+		name: "GJ 1132 b",
+		distance: "41 light-years from Earth",
+		description:
+			"GJ 1132 b may have begun as a mini-Neptune, but is now a rocky world a little bigger than Earth. The planet may have lost one atmosphere but gained another from volcanic activity.",
+		image: "https://cdn24.img.ria.ru/images/07e5/03/0c/1600904124_0:0:3640:2048_1920x0_80_0_0_62e0818bb1d36eb6a74747e919e3fc0a.jpg",
+	},
+	{
+		id: 4,
+		name: "Kepler-452 b",
+		distance: "1,799 light-years from Earth",
+		description:
+			"An 'Earth-cousin' that orbits a star like our sun in the habitable zone, where liquid water could exist.",
+		image: "https://www.vladtime.ru/uploads/posts/2017-12/thumbs/1513598132_4_02.jpg",
+	},
+	{
+		id: 5,
+		name: "GJ 504 b",
+		distance: "57 light-years from Earth",
+		description:
+			"If we could travel to this giant planet, we would see a world still glowing from the heat of its formation with a color reminiscent of a dark cherry blossom, a dull magenta.",
+		image: "https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/91a0190d-4805-4e61-8334-867cb5bb944e/d6ikmke-eb7cc127-ab83-46bc-9ac3-ae2f84bfa439.png/v1/fill/w_1280,h_720,strp/planet_gj_504_b_by_shanedk_d6ikmke-fullview.png?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7ImhlaWdodCI6Ijw9NzIwIiwicGF0aCI6IlwvZlwvOTFhMDE5MGQtNDgwNS00ZTYxLTgzMzQtODY3Y2I1YmI5NDRlXC9kNmlrbWtlLWViN2NjMTI3LWFiODMtNDZiYy05YWMzLWFlMmY4NGJmYTQzOS5wbmciLCJ3aWR0aCI6Ijw9MTI4MCJ9XV0sImF1ZCI6WyJ1cm46c2VydmljZTppbWFnZS5vcGVyYXRpb25zIl19.d-fmbcZzNhIvpGdziAMPOZZ4wWQhjO0uru9QTcb87_s",
+	},
+];
